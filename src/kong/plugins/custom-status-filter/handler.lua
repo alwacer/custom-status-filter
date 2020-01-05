db358fd4f6d0a927bb42efec63b3205ec106b6b6
YS Foo <alwacer@gmail.com>
2020-01-05 23:53:08 +0800

    clean up local function

--- src/kong/plugins/custom-status-filter/handler.lua
+++ src/kong/plugins/custom-status-filter/handler.lua
@@ -27,70 +27,65 @@ function CustomStatusFilterHandler:new()
   CustomStatusFilterHandler.super.new(self, "CustomStatusFilter")
 end
 
-
-function CustomStatusFilterHandler:header_filter(conf)
-   CustomStatusFilterHandler.super.header_filter(self)
-   local status = kong.response.get_status()
-
-   if not is_valid_status(status) then
-      kong.response.set_status(transform_custom_status_codes(status))
-   end
-   
-   -- remove content length to prevent client from waiting for the original content length to download
-   kong.response.clear_header("Content-Length")
-   kong.response.set_header("X-RWS-SOURCE", kong.response.get_source())
-   
-   --setup a uuid for error log correlation
-   local uuid = "RWS-"..uuid()
-   self.log_uuid = uuid
-   
-   kong.response.set_header("X-RWS-LOGREFID", uuid)
- 
-end  
-
-local function is_valid_status(status_code) 
-  if VALID_STATUS_CODE[status_code] == true then
-    return true
-  else  
-    return false
-  end  
-end  
+local function is_2XX (status_code)
+  return (VALID_STATUS_CODE[status_code] == true)
+end
 
 local function transform_custom_status_codes(status_code)
   -- Non-standard 4XX HTTP codes will be returned as 400 Bad Request
-  if status_code > 400 and status < 500 then
+  if status_code > 400 and status_code < 500 then
     status_code = 400
   elseif status_code >= 500 then
     status_code = 500
-  end  
-
+  end
   return status_code
 end
 
-function CustomStatusFilterHandler:body_filter(conf)
-  CustomStatusFilterHandler.super.body_filter(self)
-  local status  = transform_custom_status_codes(kong.response.get_status())
+function CustomStatusFilterHandler:header_filter(conf)
+  CustomStatusFilterHandler.super.header_filter(self)
+  local status = kong.response.get_status()
 
-  if not is_valid_status(status) then
-     local ctx = ngx.ctx
-     local chunk, eof = ngx.arg[1], ngx.arg[2]
-
-     ctx.rt_body_chunks = ctx.rt_body_chunks or {}
-     ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
-
-     if eof then
-        local chunks = table.concat(ctx.rt_body_chunks)
-        local logRefStr = '"LOG_REF_ID" : "'..self.log_uuid..'"'
-        local body = '{"Exception" : "'.. DEFAULT_RESPONSE[status] ..'"}'
-        kong.log.err('['..self.log_uuid..'],'..chunks)
-        ngx.arg[1] = body 
-
-     else
-        ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
-        ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
-        ngx.arg[1] = nil
-     end --eof
+  if not is_2XX(status) then
+     kong.response.set_status(transform_custom_status_codes(status))
   end
+
+  -- remove content length to prevent client from waiting for the original content length to download
+  kong.response.clear_header("Content-Length")
+  kong.response.set_header("X-RWS-SOURCE", kong.response.get_source())
+
+  --setup a uuid for error log correlation
+  local uuid = "RWS-"..uuid()
+  self.log_uuid = uuid
+
+  kong.response.set_header("X-RWS-LOGREFID", uuid)
+
+end
+
+function CustomStatusFilterHandler:body_filter(conf)
+ CustomStatusFilterHandler.super.body_filter(self)
+ local status  = transform_custom_status_codes(kong.response.get_status())
+
+ --filter error messages return from upstream 
+ if not is_2XX(status) then 
+    local ctx = ngx.ctx
+    local chunk, eof = ngx.arg[1], ngx.arg[2]
+
+    ctx.rt_body_chunks = ctx.rt_body_chunks or {}
+    ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
+
+    if eof then
+       local chunks = table.concat(ctx.rt_body_chunks)
+       local logRefStr = '"LOG_REF_ID" : "'..self.log_uuid..'"'
+       local body = '{"Exception" : "'.. DEFAULT_RESPONSE[status] ..'"}'
+       kong.log.err('['..self.log_uuid..'],'..chunks)
+       ngx.arg[1] = body
+
+    else
+       ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
+       ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
+       ngx.arg[1] = nil
+    end --eof
+ end
 end -- func
 
 
